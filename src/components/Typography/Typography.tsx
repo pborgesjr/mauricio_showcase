aeb89db6b26c341087796d3ae5eeac307302f974
Pedro Borges <ppmbjr@gmail.com>
2022-07-19 20:29:01 -0300

    chore: creating Typography component to handle texts

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -5,8 +5,25 @@ import styles from "./styles.module.scss";
 interface TypographyProps {
   text: string;
   type: "title" | "body";
+  verticalOrientation?: boolean;
+  customStyles?: string;
 }
 
-export const Typography = ({ text, type }: TypographyProps) => {
-  return <span className={`${styles.text} ${styles[type]}`}>{text}</span>;
+export const Typography = ({
+  text,
+  type,
+  verticalOrientation,
+  customStyles,
+}: TypographyProps) => {
+  return (
+    <div
+      className={`${styles.container} ${
+        verticalOrientation && styles.vertical
+      }`}
+    >
+      <span className={`${styles.text} ${styles[type]} ${customStyles}`}>
+        {text}
+      </span>
+    </div>
+  );
 };
