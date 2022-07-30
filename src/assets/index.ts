bb3aedd34900211d7ddb7fa42eadbe2d9714627a
Pedro Borges <ppmbjr@gmail.com>
2022-07-30 19:53:04 -0300

    chore: adição de imagens mock

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import app_logo from './images/app_logo.png';
+import big_image from './images/bigImage.jpeg';
+import mauricio_face from './images/mauricio_face.jpg';
+import bedroom from './images/bedroom.jpg';
+import fake_player from './images/fake_player.jpg';
+import area_gourmet from './images/area_gourmet.webp';
+import fachada_casa from './images/fachada_casa.png'
+import fachada_casa_2 from './images/fachada_casa_2.jpg'
+import sala from './images/sala.jpg'
+
+export { app_logo, big_image, mauricio_face, bedroom, fake_player, area_gourmet, fachada_casa, fachada_casa_2, sala };
