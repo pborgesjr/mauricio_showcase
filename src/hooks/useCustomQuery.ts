7632da57fcd69af74640a7027e5ee00273441bde
Pedro Borges <ppmbjr@gmail.com>
2022-08-29 17:10:14 -0300

    refactor: added correct import path

--- src/hooks/useCustomQuery.ts
+++ src/hooks/useCustomQuery.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { fetchAll } from "../services/fetch";
+import { fetchAll } from "../services";
 
 export const useCustomQuery = (queryKey: string, enabled: boolean = true) => {
   return useQuery(queryKey, () => fetchAll(queryKey), { enabled });
