ba6e591d9af671ae69cb1894ec87a0a296039954
Pedro Borges <ppmbjr@gmail.com>
2023-11-08 01:34:09 -0300

    hotfix: adding tsignore

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -19,6 +19,7 @@ export const Projects = () => {
 
   const handleSelectProject = async (path: string) => {
     /* setSelectedProject(path); */
+    //@ts-ignore
     modalRef?.current?.openModal();
   };
 
